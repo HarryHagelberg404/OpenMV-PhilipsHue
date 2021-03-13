4e0eb7236fb3fc02b231dba0dc505a1bba01c831
Harry Hagelberg <hh222wf@student.lnu.se>
2021-03-13 16:07:00 +0100

    adding stream area

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,9 +1,20 @@
 function Main() {
-    return (
-      <>
-        <h1>Main</h1>
-      </>
-    );
-  }
-  
-  export default Main;
+  return (
+    <>
+      <div className="Main">
+        <div className="Stream">
+          <video className="Content" autoPlay playsInline loop muted>
+            <source src="./hamster.mp4" type="video/mp4" />
+          </video>
+        </div>
+        <div className="Info">
+          <h1>Information</h1>
+          <p>Information</p>
+          <p>Information</p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Main;
