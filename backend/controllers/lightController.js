aa194dc3096176220684a3740b7ab010a4e64430
Harry Hagelberg <hh222wf@student.lnu.se>
2021-03-17 10:03:36 +0100

    initing backend

---
+++ backend/controllers/lightController.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const lightController = {}
+
+/**
+ * Renders our 'homepage'.
+ *
+ * @param  {object} req Is the request object.
+ * @param  {object} res Is the response object.
+ */
+lightController.index = (req, res) => {
+    console.log("Gotcha")
+}
+
+module.exports = lightController
