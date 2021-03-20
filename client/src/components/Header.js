eb8c78078bc32cfabac5f91bbddaa89381a9ca7b
Harry Hagelberg <hh222wf@student.lnu.se>
2021-03-20 12:34:43 +0100

    updating css

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,12 +1,14 @@
 import { Link } from "react-router-dom";
 
-export default function Header() {return (
+export default function Header() {
+  return (
     <header className="Header">
       <nav>
         <ul>
           <Link to="/">
             <li>
-                <p>Light.AI</p>
+              <p className="Header-headline">Light.AI</p>
+              <p>An interface controlling your smart lamps</p>
             </li>
           </Link>
         </ul>
